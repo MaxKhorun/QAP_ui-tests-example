0c393b4c7e8089204d55dfe8a677c39779a62d02
MaxKhorun <sea-of-max@yandex.ru>
2023-09-29 05:37:21 +0300

    Updated and resorted

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -5,8 +5,8 @@ from settings import login_email, login_passw
 def test_authorisation(web_browser):
 
     page = AuthPage(web_browser)
-    page.email.send_keys(login_email)
-    page.password.send_keys(login_passw)
+    page.email.send_keys('maxim@mail.ru')
+    page.password.send_keys('1234')
     page.btn.click()
 
     assert page.get_current_url() == 'https://petfriends.skillfactory.ru/all_pets'
